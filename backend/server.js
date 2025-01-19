bc13b5351148dfc40d9db61720cbe66d8486707c
Adnan Yasir <mr.adnan.yasir@gmail.com>
2025-01-19 05:34:57 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -33,3 +33,9 @@ server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
+
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
